aa215ed9bdad18decf07a0e6a4c1607452a3c50d
rafat962 <rafatkamel96@gmail.com>
2025-10-04 20:39:25 +0300

    attachments

--- src/components/Dashboard/FormMap.jsx
+++ src/components/Dashboard/FormMap.jsx
@@ -11,10 +11,64 @@ import { Controller, useForm } from "react-hook-form";
 import { useMapContext } from "../../shared/Context/MapContext";
 import { Parcels } from "../../shared/static/StaticMapData";
 import toast from "react-hot-toast";
-
+import { Grid, Card, CardMedia, CardActions, IconButton } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import Graphic from "@arcgis/core/Graphic";
 const FormMap = () => {
     const { state } = useMapContext();
     const { data } = state;
+    const handleFilesChange = (e, field) => {
+        const files = e.target.files ? Array.from(e.target.files) : [];
+        // دمج القديم مع الجديد
+        const newFiles = [...(field.value || []), ...files];
+        field.onChange(newFiles);
+
+        // Create preview URLs
+        const urls = newFiles.map((file) => URL.createObjectURL(file));
+        setPreviews(urls);
+    };
+
+    const handleRemove = (index, field, imageId) => {
+        const newFiles = [...(field.value || [])];
+        newFiles.splice(index, 1); // remove file
+        field.onChange(newFiles);
+
+        const newPreviews = [...previews];
+        newPreviews.splice(index, 1); // remove preview
+        setPreviews(newPreviews);
+        if (imageId) {
+            let feature = new Graphic({
+                attributes: {
+                    OBJECTID: data.OBJECTID,
+                },
+            });
+            Parcels.deleteAttachments(feature, [imageId])
+                .then((res) => {
+                    toast.success("Deleted Successfully");
+                })
+                .catch((err) => {
+                    toast.err(err.message);
+                });
+        }
+    };
+    async function getAttachment(id) {
+        await Parcels.queryAttachments({
+            where: `OBJECTID = ${id}`,
+        }).then((result) => {
+            let urls = [];
+            Object.values(result).forEach((array) => {
+                array.forEach((x) => {
+                    urls.push({
+                        id: x.id,
+                        url: x.url,
+                    });
+                });
+            });
+            setPreviews(urls);
+        });
+    }
+
     useEffect(() => {
         if (data) {
             const sanitized = {
@@ -26,8 +80,11 @@ const FormMap = () => {
                 Status: data?.Status ?? "",
                 Parcel_View: data?.Parcel_View ?? "",
                 Land_Terrain: data?.Land_Terrain ?? "",
+                images: [],
             };
+            getAttachment(data?.OBJECTID);
             reset(sanitized);
+            setPreviews([]);
         }
     }, [data]);
     const { control, handleSubmit, reset } = useForm({
@@ -41,9 +98,11 @@ const FormMap = () => {
             Status: "",
             Parcel_View: "",
             Land_Terrain: "",
+            images: [],
         },
     });
     const [open, setOpen] = useState(false);
+    const [previews, setPreviews] = useState([]);
     function onSuccess(Formdata) {
         if (!data || Object.keys(data).length === 0) return;
         if (!Formdata) return;
@@ -64,8 +123,27 @@ const FormMap = () => {
         Parcels.applyEdits({
             updateFeatures: [updatedFeature],
         })
-            .then((result) => {
-                console.log(reset);
+            .then(async (result) => {
+                const objectId = result.updateFeatureResults[0].objectId;
+                // attachment
+                if (Formdata.images && Formdata.images.length > 0) {
+                    for (const file of Formdata.images) {
+                        const formData = new FormData();
+                        formData.append("attachment", file);
+                        let finalUrl = `${Parcels.url}/0/${Formdata.OBJECTID}/addAttachment`;
+                        await fetch(finalUrl, {
+                            method: "POST",
+                            body: formData,
+                        }).then((response) => {
+                            console.log(response);
+                            if (!response.ok) {
+                                throw new Error(
+                                    `Failed to upload attachment: ${response.statusText}`
+                                );
+                            }
+                        });
+                    }
+                }
                 toast.success("parcels Update Successfully");
                 setOpen(false);
             })
@@ -75,7 +153,7 @@ const FormMap = () => {
             });
     }
     return (
-        <div className=" container w-full h-full space-y-4">
+        <div className=" container w-full h-full space-y-4 overflow-auto">
             {/* main container */}
             <div className="w-full border-b-2 border-b-gray-300 shadow-2xs flex items-center justify-center p-2">
                 <p className="font-thr text-md tracking-wide font-bold">
@@ -300,6 +378,91 @@ const FormMap = () => {
                         )}
                     />
                 </div>
+                {/* Images Upload with Preview + Remove */}
+                <div className="min-w-full mb-6">
+                    <Controller
+                        key="images"
+                        name="images"
+                        control={control}
+                        defaultValue={[]} // مهم
+                        render={({ field }) => {
+                            return (
+                                <>
+                                    <input
+                                        accept="image/*"
+                                        multiple
+                                        type="file"
+                                        id="images-upload"
+                                        style={{ display: "none" }}
+                                        onChange={(e) =>
+                                            handleFilesChange(e, field)
+                                        }
+                                    />
+                                    <label htmlFor="images-upload">
+                                        <Button
+                                            variant="contained"
+                                            component="span"
+                                            fullWidth
+                                            startIcon={<CloudUploadIcon />}
+                                        >
+                                            Upload Images
+                                        </Button>
+                                    </label>
+
+                                    {/* Show Previews with Remove Button */}
+                                    <Grid
+                                        container
+                                        spacing={1}
+                                        className="mt-2"
+                                    >
+                                        {previews.length > 0 &&
+                                            previews.map((src, index) => (
+                                                <Grid
+                                                    item
+                                                    xs={6}
+                                                    sm={4}
+                                                    md={3}
+                                                    key={index}
+                                                >
+                                                    <Card>
+                                                        <CardMedia
+                                                            component="img"
+                                                            image={
+                                                                src?.url || src
+                                                            }
+                                                            alt={`preview-${index}`}
+                                                            style={{
+                                                                objectFit:
+                                                                    "cover",
+                                                                height: "80px",
+                                                                width: "120px",
+                                                            }}
+                                                        />
+                                                        <CardActions>
+                                                            <IconButton
+                                                                color="error"
+                                                                onClick={() =>
+                                                                    handleRemove(
+                                                                        index,
+                                                                        field,
+                                                                        src?.id
+                                                                    )
+                                                                }
+                                                            >
+                                                                <DeleteIcon />
+                                                            </IconButton>
+                                                        </CardActions>
+                                                    </Card>
+                                                </Grid>
+                                            ))}
+                                    </Grid>
+                                </>
+                            );
+                        }}
+                    />
+                </div>
+
+                {/* update data */}
                 <Button type="submit" className="w-full" variant="contained">
                     <p className="text-lg">Update Data</p>
                 </Button>
